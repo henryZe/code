be44cd32b754f5bdebbd0db95324b49ff130488d
Zeng Heng <henryZ_e@163.com>
2020-09-24 21:37:55 +0800

    update week6

--- python_study/week_6.py
+++ python_study/week_6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-if 1:
+if 0:
     # set feature:
     # A. unique
     # B. un-sort
@@ -11,5 +11,49 @@ if 1:
     B = set("pypy123")
     print(B)
 
+    # '-', '&', '|', '^'
+    A = {"p", "y", 123}
+    B = set("pypy123")
+    print(A - B)
+    print(B - A)
+    print(A & B)
+    print(A | B)
+    print(A ^ B)
 
+if 0:
+    A = {"p", "y", 123}
+
+    # add discard remove clear pop
+    # copy len set in not in
+    for i in A:
+        print(i)
+
+    try:
+        while True:
+            print(A.pop())
+    except:
+        pass
+
+if 0:
+    # discard redundant data
+    ls = ["p", "y", "p", "y", 123]
+    temp = set(ls)
+    ls = list(temp)
+    print(ls)
+
+if 0:
+    # array
+    # in not in + * [index] [i:j:k]
+    ls = ["python", 123, ".io"]
+    print(ls[::-1])
+    print(len(ls))
+    print(ls.count(123))
+
+    str = "python123.io"
+    print(min(str))
+    print(max(str))
+    print(str.index("1"))
+    print(str.count("o"))
+
+if 1:
 
