2ed4444bfc6458f52cb8e656a2c2b0051d8f5252
Zeng Heng <zeng.heng@wuqi-tech.com>
2020-09-27 14:06:00 +0800

    add week 7

---
+++ python_study/week_7.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+if 0:
+    fd = open("output.txt", "w+")
+    ls = ["中国", "法国", "美国"]
+    fd.writelines(ls)
+    fd.seek(0)
+    for line in fd:
+        print(line)
+    fd.close()
+
+if 1:
+    import turtle as t
+
+    t.title("自动轨迹绘制")
+    t.setup(800, 600, 0, 0)
+    t.pencolor("red")
+    t.pensize(5)
+
+    datals = []
+    f = open("data.txt")
+    for line in f:
+        line = line.replace("\n", "")
+        datals.append(list(map(eval, line.split(","))))
+    f.close()
+
+    for i in range(len(datals)):
+        t.speed(1)
+        t.pencolor(datals[i][3], datals[i][4], datals[i][5])
+        t.fd(datals[i][0])
+        if datals[i][1]:
+            t.right(datals[i][2])
+        else:
+            t.left(datals[i][2])
+    t.done()
