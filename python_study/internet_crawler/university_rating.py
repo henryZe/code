d862279b4f39b865792a3e7a4e0bd2a4273f1f60
zeng heng <henryz_e@163.com>
2020-12-01 01:01:38 +0800

    update university_rating

--- python_study/internet_crawler/university_rating.py
+++ python_study/internet_crawler/university_rating.py
@@ -5,3 +5,48 @@ Created on Mon Nov 30 22:13:00 2020
 @author: user
 """
 
+import requests
+from bs4 import BeautifulSoup
+import bs4
+import re
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout = 10)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+
+    except:
+        print("cause exception")
+        return ""
+
+def fillUnivList(ulist, html):
+    soup = BeautifulSoup(html, "html.parser")
+    
+    for tr in soup.find("tbody").children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            # rating, university, city, sort, score
+            ulist.append([tds[0].text.strip(),
+                          tds[1].text.strip(),
+                          tds[2].text.strip(),
+                          tds[3].text.strip(),
+                          tds[4].text.strip()])
+
+def printUnivList(ulist, num):
+    f = "{:^10}\t{:^20}\t{:^10}"
+    print(f.format("Rating", "University", "Score"))
+    for i in range(num):
+        u = ulist[i]
+        print(f.format(u[0], u[1], u[4]))
+
+def main():
+    uinfo = []
+    url = "https://www.shanghairanking.cn/rankings/bcur/2020"
+
+    html = getHTMLText(url)
+    fillUnivList(uinfo, html)
+    printUnivList(uinfo, 30)
+
+main()
