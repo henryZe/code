be99d90af69d7d5e74d651a3ac101df66f1d92ef
Zeng Heng <henryZ_e@163.com>
2020-12-21 22:18:55 +0800

    control ball

--- python_study/vdgame_dev/game_ball.py
+++ python_study/vdgame_dev/game_ball.py
@@ -9,11 +9,36 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Ball 1.0")
 ball = pygame.image.load("PYG02-ball.gif")
 ballrect = ball.get_rect()
+# frames per second
+fps = 300
+fclock = pygame.time.Clock()
+
+def sign(num):
+    if num < 0:
+        return -1
+    if num > 0:
+        return 1
+    if num == 0:
+        return 0
+
+def speed_tone(axis, direct):
+    if (abs(speed[axis]) + direct) == 0:
+        return
+    speed[axis] = (abs(speed[axis]) + direct) * sign(speed[axis])
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed_tone(0, -1)
+            if event.key == pygame.K_RIGHT:
+                speed_tone(0, 1)
+            if event.key == pygame.K_UP:
+                speed_tone(1, -1)
+            if event.key == pygame.K_DOWN:
+                speed_tone(1, 1)
 
     ballrect = ballrect.move(speed[0], speed[1])
     if ballrect.left < 0 or ballrect.right > width:
@@ -24,3 +49,4 @@ while True:
     screen.fill(BLACK)
     screen.blit(ball, ballrect)
     pygame.display.update()
+    fclock.tick(fps)
