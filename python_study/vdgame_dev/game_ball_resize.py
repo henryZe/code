5bd477990bf699c0e4b69dbcfc8077855665d32a
Zeng Heng <henryZ_e@163.com>
2020-12-22 11:10:40 +0800

    fullscreen version

--- python_study/vdgame_dev/game_ball_resize.py
+++ python_study/vdgame_dev/game_ball_resize.py
@@ -2,10 +2,14 @@ import pygame, sys
 
 pygame.init()
 
-size = width, height = 1600, 900
 speed = [1, 1]
 BLACK = 0, 0, 0
-screen = pygame.display.set_mode(size)
+vInfo = pygame.display.Info()
+size = width, height = vInfo.current_w, vInfo.current_h
+# screen = pygame.display.set_mode(size)
+# screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+# screen = pygame.display.set_mode(size, pygame.NOFRAME)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 pygame.display.set_caption("Ball 1.0")
 ball = pygame.image.load("PYG02-ball.gif")
 ballrect = ball.get_rect()
@@ -33,12 +37,14 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 speed_tone(0, -1)
-            if event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:
                 speed_tone(0, 1)
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 speed_tone(1, -1)
-            if event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:
                 speed_tone(1, 1)
+            elif event.key == pygame.K_ESCAPE:
+                sys.exit()
 
     ballrect = ballrect.move(speed[0], speed[1])
     if ballrect.left < 0 or ballrect.right > width:
