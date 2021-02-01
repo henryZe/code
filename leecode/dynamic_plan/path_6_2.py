8e56be18cb5c355a08fcd7a924ec4108f5c0714d
zeng heng <henryz_e@163.com>
2021-02-02 00:38:25 +0800

    update note

--- leecode/dynamic_plan/path_6_2.py
+++ leecode/dynamic_plan/path_6_2.py
@@ -1,28 +1,34 @@
 import numpy as np
+from scipy.special import comb
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # def search(num:int, i: int, j: int) -> int:
-        #     if i >= m or j >= n:
-        #         return num
+        if 0:
+            def search(num:int, i: int, j: int) -> int:
+                if i >= m or j >= n:
+                    return num
 
-        #     if i == m - 1 and j == n - 1:
-        #         num += 1
-        #         return num
+                if i == m - 1 and j == n - 1:
+                    num += 1
+                    return num
 
-        #     num = search(num, i + 1, j)
-        #     num = search(num, i, j + 1)
+                num = search(num, i + 1, j)
+                num = search(num, i, j + 1)
 
-        #     return num
+                return num
 
-        # return search(0, 0, 0)
+            return search(0, 0, 0)
 
-        f = np.ones((m, n), dtype=int)
-        for i in range(1, m):
-            for j in range(1, n):
-                f[i][j] = f[i-1][j] + f[i][j-1]
+        if 1:
+            f = np.ones((m, n), dtype=int)
+            for i in range(1, m):
+                for j in range(1, n):
+                    f[i][j] = f[i-1][j] + f[i][j-1]
 
-        return f[m-1][n-1]
+            return f[m-1][n-1]
+        
+        if 0:
+            return comb(m + n - 2, m - 1)
 
 obj = Solution()
 print(obj.uniquePaths(20, 12))
