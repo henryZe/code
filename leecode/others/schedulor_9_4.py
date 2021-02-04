1ace41f23606a0fcf044331bcfb0672aaef2987b
zeng heng <henryz_e@163.com>
2021-02-05 01:20:42 +0800

    update code

--- leecode/others/schedulor_9_4.py
+++ leecode/others/schedulor_9_4.py
@@ -1,12 +1,34 @@
 from typing import List
+import collections
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        # dict
+        freq = collections.Counter(tasks)
 
+        m = len(freq)
+        nextValid = [1] * m
+        # dict key:value
+        rest = list(freq.values())
 
+        time = 0
+        for i in range(len(tasks)):
+            # time counter
+            time += 1
+            minValid = min(nextValid[j] for j in range(m) if rest[j] > 0)
+            time = max(minValid, time)
 
+            # pick max rest one
+            best = -1
+            for j in range(m):
+                if nextValid[j] <= time and rest[j] > 0:
+                    if best == -1 or rest[j] > rest[best]:
+                        best = j
 
-        return 0
+            nextValid[best] = time + n + 1
+            rest[best] -= 1
+
+        return time
 
 
 tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"]
