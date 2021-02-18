341b3110bdd54bd4a289c22d59791a8fe2fef51f
Zeng Heng <henryZ_e@163.com>
2021-02-18 21:30:26 +0800

    add now_coder/test93.py

---
+++ leecode/now_coder/test93.py
@@ -0,0 +1,11 @@
+class Solution:
+    def LRU(self , operators , k ):
+        # write code here
+        a = input("input a: ")
+        print(a)
+        return
+
+
+operators = [[1,1,1],[1,2,2],[1,3,2],[2,1],[1,4,4],[2,2]]
+k = 3
+Solution().LRU(operators, k)
