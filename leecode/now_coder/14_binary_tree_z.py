a54e4f9c5a96c1373f83d991a9f672c22c6a16c8
Zeng Heng <henryZ_e@163.com>
2021-02-20 20:11:52 +0800

    add now_coder/14_binary_tree_z.py

---
+++ leecode/now_coder/14_binary_tree_z.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+
+
+class Solution:
+    def zigzagLevelOrder(self , root ):
+        # write code here
+
+
+
