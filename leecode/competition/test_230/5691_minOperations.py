8d4a2c7b5da7ad49c880f283428cc742f7cd8074
zeng heng <zeng.heng@wuqi-tech.com>
2021-03-03 15:57:46 +0800

    add 5691

--- leecode/competition/test_230/5691_minOperations.py
+++ leecode/competition/test_230/5691_minOperations.py
@@ -1,18 +1,29 @@
-from typing import List
+from typing import Counter, List
 
+# 贪心算法
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
-        len1 = len(nums1)
-        len2 = len(nums2)
+        sum1, sum2 = sum(nums1), sum(nums2)
+        if sum1 == sum2:
+            return 0
+        if sum1 > sum2:
+            return self.minOperations(nums2, nums1)
 
-        if abs(len1 - len2) > 5:
-            return -1
-
+        diff = sum2 - sum1
+        freq = Counter(6 - num for num in nums1) + Counter(num - 1 for num in nums2)
         res = 0
 
+        for i in range(5, 0, -1):
+            if diff <= 0:
+                break
+            for _ in range(freq[i]):
+                if diff <= 0:
+                    break
+                diff -= i
+                freq[i] -= 1
+                res += 1
 
-
-        return res
+        return res if diff <= 0 else -1
 
 nums1 = [1,2,3,4,5,6]
 nums2 = [1,1,2,2,2,2]
