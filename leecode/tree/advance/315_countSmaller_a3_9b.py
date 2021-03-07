4faa4dc54c9747da0c529053ea4387d77eeaedbc
zeng heng <zeng.heng@wuqi-tech.com>
2021-03-07 14:05:37 +0800

    add 315 fenwick tree

--- leecode/tree/advance/315_countSmaller_a3_9b.py
+++ leecode/tree/advance/315_countSmaller_a3_9b.py
@@ -1,5 +1,28 @@
 from typing import List
-import bisect
+import heapq
+
+class fenwickTree:
+    def __init__(self, size: int):
+        self.size = size
+        self.tree = [0 for _ in range(size + 1)]
+
+    def __str__(self):
+        return "size: {} arr: {}".format(self.size, self.tree)
+
+    def lowbit(self, x: int) -> int:
+        return x & (-x)
+
+    def update(self, index: int, delta: int):
+        while index <= self.size:
+            self.tree[index] += delta
+            index += self.lowbit(index)
+
+    def query(self, index: int) -> int:
+        res = 0
+        while index:
+            res += self.tree[index]
+            index -= self.lowbit(index)
+        return res
 
 # O(n * logn)
 class Solution:
@@ -7,15 +30,24 @@ class Solution:
         if not nums:
             return []
 
-        size = len(nums)
-        counts = [0] * size
-        ordered = []
+        num_set = list(set(nums))
+        heapq.heapify(num_set)
+        num_dict = {}
+        rank = 1
+        for i in range(len(num_set)):
+            # 离散化数组，缩小数据规模
+            num = heapq.heappop(num_set)
+            num_dict[num] = rank
+            rank += 1
+        
+        tree = fenwickTree(len(num_dict))
+        res = [0 for _ in range(len(nums))]
+        for i in range(len(nums) - 1, -1, -1):
+            rank = num_dict[nums[i]]
+            res[i] = tree.query(rank - 1)
+            tree.update(rank, 1)
 
-        for i in range(size - 1, -1, -1):
-            counts[i] = bisect.bisect_left(ordered, nums[i])
-            ordered.insert(counts[i], nums[i])
-
-        return counts
+        return res
 
 
 nums = [5,2,6,1]
