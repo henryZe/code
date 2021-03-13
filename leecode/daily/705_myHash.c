5a17ff56a09c9bbb8486774bd625d68f3f04563d
zeng heng <zeng.heng@wuqi-tech.com>
2021-03-14 00:32:07 +0800

    add 705

--- leecode/daily/705_myHash.c
+++ leecode/daily/705_myHash.c
@@ -1,52 +1,122 @@
-#include <>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// 链地址法, base 为质数
+#define base 769
+
+struct List {
+    int val;
+    struct List *next;
+};
 
 typedef struct {
-
+    struct List *data;
 } MyHashSet;
 
-/** Initialize your data structure here. */
-
-MyHashSet* myHashSetCreate() {
-
+int hash(int key)
+{
+    return key % base;
 }
 
-void myHashSetAdd(MyHashSet* obj, int key) {
-
+bool listContains(struct List *head, int key)
+{
+    for (struct List *it = head; it->next; it = it->next) {
+        // printf("it->val = %d key = %d\n", it->val, key);
+        if (it->next->val == key)
+            return true;
+    }
+    return false;
 }
 
-void myHashSetRemove(MyHashSet* obj, int key) {
+void listPush(struct List *head, int key)
+{
+    struct List *tmp = malloc(sizeof(struct List));
+    tmp->val = key;
+    tmp->next = head->next;
+    head->next = tmp;
+}
 
+void listDelete(struct List *head, int key)
+{
+    struct List *tmp;
+
+    for (struct List *it = head; it->next; it = it->next) {
+        if (it->next->val == key) {
+            tmp = it->next;
+            it->next = tmp->next;
+            free(tmp);
+            break;
+        }
+    }
+}
+
+void listFree(struct List *head)
+{
+    struct List *tmp;
+
+    while (head->next) {
+        tmp = head->next;
+        head->next = tmp->next;
+        free(tmp);        
+    }
+}
+
+MyHashSet* myHashSetCreate(void)
+{
+    MyHashSet *ret = malloc(sizeof(MyHashSet));
+    
+    ret->data = malloc(sizeof(struct List) * base);
+    for (int i = 0; i < base; i++) {
+        ret->data[i].val = 0;
+        ret->data[i].next = NULL;
+    }
+
+    return ret;
+}
+
+void myHashSetAdd(MyHashSet* obj, int key)
+{
+    int h = hash(key);
+    if (!listContains(&(obj->data[h]), key))
+        listPush(&(obj->data[h]), key);
+}
+
+void myHashSetRemove(MyHashSet* obj, int key)
+{
+    int h = hash(key);
+    listDelete(&(obj->data[h]), key);
 }
 
 /** Returns true if this set contains the specified element */
-bool myHashSetContains(MyHashSet* obj, int key) {
-
+bool myHashSetContains(MyHashSet* obj, int key)
+{
+    int h = hash(key);
+    return listContains(&(obj->data[h]), key);
 }
 
-void myHashSetFree(MyHashSet* obj) {
+void myHashSetFree(MyHashSet* obj)
+{
+    for (int i = 0; i < base; i++)
+        listFree(&(obj->data[i]));
 
+    free(obj->data);
+    free(obj);
 }
 
-/**
- * Your MyHashSet struct will be instantiated and called as such:
- * MyHashSet* obj = myHashSetCreate();
- * myHashSetAdd(obj, key);
- 
- * myHashSetRemove(obj, key);
- 
- * bool param_3 = myHashSetContains(obj, key);
- 
- * myHashSetFree(obj);
-*/
 int main(void)
 {
     MyHashSet* obj = myHashSetCreate();
-    int key = 10;
 
-    myHashSetAdd(obj, key);
-    myHashSetRemove(obj, key);
-
-    printf("contain %d ? %s\n", key, myHashSetContains(obj, key) ? "True" : "False");
+    myHashSetAdd(obj, 1);
+    myHashSetAdd(obj, 2);
+    printf("contain %d ? %s\n", 1, myHashSetContains(obj, 1) ? "True" : "False");
+    printf("contain %d ? %s\n", 3, myHashSetContains(obj, 3) ? "True" : "False");
+    
+    myHashSetAdd(obj, 2);
+    printf("contain %d ? %s\n", 2, myHashSetContains(obj, 2) ? "True" : "False");
+    myHashSetRemove(obj, 2);
+    printf("contain %d ? %s\n", 2, myHashSetContains(obj, 2) ? "True" : "False");
 
     myHashSetFree(obj);
     return 0;
