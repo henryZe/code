b133d73b865eb0ea71108d63030c5102a43a8188
zeng heng <zeng.heng@wuqi-tech.com>
2021-03-19 20:55:05 +0800

    add 92

--- leecode/daily/92_reverseBetween.py
+++ leecode/daily/92_reverseBetween.py
@@ -8,38 +8,29 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        cur = head
-        prev = None
-        headInsert = head
-        headOrigin = None
-        i = 1
+        dummyNode = ListNode(-1)
+        dummyNode.next = head
+        pre = dummyNode
+        for _ in range(left - 1):
+            pre = pre.next
 
-        while cur:
+        cur = pre.next
+        for _ in range(right - left):
             next = cur.next
+            cur.next = next.next
+            next.next = pre.next
+            pre.next = next
 
-            
-            if left <= i <= right:
-                cur.next = prev
-                
-
-            prev = cur
-            cur = next
-            i += 1
-
-        headInsert.next = cur
-        if headOrigin:
-            headOrigin.next = prev
-
-        return head
+        return dummyNode.next
 
 if __name__ == "__main__":
-    # head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-    # left = 2
-    # right = 4
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    left = 2
+    right = 4
     # head = ListNode(5)
     # left = 1
     # right = 1
-    head = ListNode(3, ListNode(5))
-    left = 1
-    right = 2
+    # head = ListNode(3, ListNode(5))
+    # left = 1
+    # right = 2
     print(Solution().reverseBetween(head, left, right))
