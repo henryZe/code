8cd0916a26a837510837981bd976a0f0cca9f0b7
zeng heng <zeng.heng@wuqi-tech.com>
2021-03-30 12:13:14 +0800

    add 74

---
+++ leecode/daily/74_searchMatrix.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = 0
+        right = m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if matrix[mid // n][mid % n] < target:
+                left = mid + 1
+            elif matrix[mid // n][mid % n] > target:
+                right = mid - 1
+            else:
+                return True
+
+        return False
+
+# matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+# target = 3
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 13
+print(Solution().searchMatrix(matrix, target))
--- leecode/mooc_data_struct/week4/homework/3_rootOfAVLTree.c
+++ leecode/mooc_data_struct/week4/homework/3_rootOfAVLTree.c
@@ -1,14 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int abs(int x)
+{
+    return x > 0 ? x : -x;
+}
 
 struct tree {
     int val;
+    int delta_degree;
     struct tree *left;
     struct tree *right;
 };
 
-int tree_insert(struct tree *t, struct tree *new)
+enum rotation {
+    LL,
+    RR,
+    LR,
+    RL,
+};
+
+void inOrder(struct tree *t)
 {
-    return 0;
+    if (!t)
+        return;
+
+    inOrder(t->left);
+    printf("%d l: %d r: %d\n", t->val,
+                t->left ? t->left->val : -1,
+                t->right ? t->right->val : -1);
+    inOrder(t->right);
+
+    return;
+}
+
+struct tree *tree_insert(struct tree *t, struct tree *new)
+{
+    if (!t)
+        return new;
+
+    if (new->val <= t->val) {
+        t->left = tree_insert(t->left, new);
+    } else if (new->val > t->val) {
+        t->right = tree_insert(t->right, new);
+    }
+
+    // update t's degree
+    // if abs(delta_degree) > 1
+    //     founder = t & troubler = new
+    //     calculate relative location of t & new
+
+    // switch(location) {
+    //     case LL:
+    //         rotate_ll
+    //     case RR:
+    //         rotate_rr
+    //     case LR:
+    //         rotate_lr
+    //     case RL:
+    //         rotate_rl
+    //     default:
+    //         break;
+    // }
+
+    // update degree
+
+    return t;
 }
 
 struct tree *create_avl_tree(int num)
@@ -27,6 +84,8 @@ struct tree *create_avl_tree(int num)
         root = tree_insert(root, &ls[i]);
     }
 
+    // display tree
+    inOrder(root);
     return root;
 }
 
