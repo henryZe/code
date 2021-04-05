f77fa67216923241c9fbdb52f806845c68279f05
zeng heng <zeng.heng@wuqi-tech.com>
2021-04-05 14:58:53 +0800

    add interview_17_21_trap_c

--- leecode/daily/interview_17_21_trap_c.py
+++ leecode/daily/interview_17_21_trap_c.py
@@ -5,27 +5,28 @@ class Solution:
     # Time O(n)
     # Space O(n)
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
+        ans = 0
+        stack = []
         n = len(height)
 
-        res = 0
-        left = 0
-        right = n - 1
-        leftMax, rightMax = 0, 0
-        while left < right:
-            # print(left, right)
-            leftMax = max(height[left], leftMax)
-            rightMax = max(height[right], rightMax)
-            if height[left] > height[right]:
-                res += rightMax - height[right]
-                right -= 1
-            else:
-                res += leftMax - height[left]
-                left += 1
-            # print("res", res)
-        return res
+        for i, h in enumerate(height):
+            # current height is higher than last one's
+            while stack and height[i] > height[stack[-1]]:
+                # print(i, stack)
+                # pop low height
+                top = stack.pop()
+                # no left any more, pass it
+                if not stack:
+                    break
+                left = stack[-1]
+                curWidth = i - left - 1
+                curHeight = min(height[left], height[i]) - height[top]
+                ans += curHeight * curWidth
+                # print(ans)
+            stack.append(i)
+        
+        # print(stack)
+        return ans
 
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 print(Solution().trap(height))
