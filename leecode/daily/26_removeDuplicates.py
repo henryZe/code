cc70ad24b36f5fd85291c449115dc2173310d981
zeng heng <zeng.heng@wuqi-tech.com>
2021-04-06 16:24:13 +0800

    add 26

---
+++ leecode/daily/26_removeDuplicates.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        pos = 0
+        for i in range(1, n):
+            if nums[i] != nums[i - 1]:
+                pos += 1
+                nums[pos] = nums[i]
+
+        return pos + 1
+
+nums = [1,1,2]
+num = Solution().removeDuplicates(nums)
+for i in range(num):
+    print(nums[i])
