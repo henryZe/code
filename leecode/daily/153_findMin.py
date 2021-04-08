0f09d062efee3681e551c8d0bd24cde97403c3f5
zeng heng <zeng.heng@wuqi-tech.com>
2021-04-08 15:12:52 +0800

    add 153

---
+++ leecode/daily/153_findMin.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    # O(logn)
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            print(mid)
+            if nums[mid] < nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        return nums[l]
+
+nums = [3,4,5,1,2]
+print(Solution().findMin(nums))
