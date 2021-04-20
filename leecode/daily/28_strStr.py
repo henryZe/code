5c0faa496729a09f508cc4a9d00740fa55fdb651
zeng heng <zeng.heng@wuqi-tech.com>
2021-04-20 22:10:05 +0800

    add 28

--- leecode/daily/28_strStr.py
+++ leecode/daily/28_strStr.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     # TYTS
     # O(n * m)
@@ -17,26 +19,58 @@ class Solution:
     #             break
     # 
     #     return res
-    
+
     # Knuth-Morris-Pratt KMP 算法
     # time O(n+m)
     # space O(m)
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-    
-        if not haystack:
-            return -1
+
+        def partialMatchTable(pattern: str) -> List[int]:
+            length = len(pattern)
+            pmt = [0] * length
+
+            for i in range(1, length):
+                k = pmt[i - 1]
+                while k > 0 and pattern[i] != pattern[k]:
+                    k = pmt[k - 1]
+                if pattern[i] == pattern[k]:
+                    pmt[i] = k + 1
+
+            return pmt
 
         n = len(haystack)
         m = len(needle)
 
-        return True
+        if not needle:
+            return 0
+
+        if n < m:
+            return -1
+
+        pmt = partialMatchTable(needle)
+        i = j = 0
+        while i < n:
+            if haystack[i] == needle[j]:
+                if j == m - 1:
+                    # finish
+                    return i - j
+                i += 1
+                j += 1
+
+            else:
+                if j > 0:
+                    j = pmt[j - 1]
+                else:
+                    i += 1
+
+        return -1
 
 # haystack = "hello"
 # needle = "ll"
 # haystack = "aaaaa"
 # needle = "bba"
-haystack = ""
-needle = ""
+# haystack = ""
+# needle = ""
+haystack = "mississippi"
+needle = "issipi"
 print(Solution().strStr(haystack, needle))
