c4f76973dd75864fdfcbe9ecc9d0839e87e6eff8
zeng heng <zeng.heng@wuqi-tech.com>
2021-09-07 20:04:45 +0800

    add 68

--- leecode/now_coder/68_jumpFloor.c
+++ leecode/now_coder/68_jumpFloor.c
@@ -6,11 +6,19 @@ int jumpFloor(int number)
     if (number == 0 || number == 1)
         return 1;
 
-    return jumpFloor(number - 2) + jumpFloor(number - 1);
+    int a[number + 1];
+    a[0] = 1;
+    a[1] = 1;
+
+    for (int i = 2; i <= number; i++) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+
+    return a[number];
 }
 
 int main(void)
 {
-    printf("step = %d\n", jumpFloor(4));
+    printf("step = %d\n", jumpFloor(7));
     return 0;
 }
