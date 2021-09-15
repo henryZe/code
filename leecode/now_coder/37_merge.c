2e233758a4428976c82c5d4e4cadd63965c5ad56
zeng heng <zeng.heng@wuqi-tech.com>
2021-09-15 16:36:19 +0800

    add 37

--- leecode/now_coder/37_merge.c
+++ leecode/now_coder/37_merge.c
@@ -6,12 +6,81 @@ struct Interval {
     int end;
 };
 
-struct Interval* merge(struct Interval* intervals, int intervalsLen, int* returnSize )
+int compare(const void *a1, const void *a2)
 {
+    struct Interval *a = (struct Interval *)a1;
+    struct Interval *b = (struct Interval *)a2;
 
+    if (a->start == b->start) {
+        return a->end - b->end;
+    }
+
+    return a->start - b->start;
+}
+
+void merge_two(struct Interval* cur, struct Interval* a)
+{
+    cur->end = cur->end > a->end ? cur->end : a->end;
+}
+
+struct Interval* merge(struct Interval* intervals, int intervalsLen, int* returnSize)
+{
+    if (!intervalsLen) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    qsort(intervals, intervalsLen, sizeof(struct Interval), compare);
+
+    struct Interval* ret = (struct Interval*)malloc(intervalsLen * sizeof(struct Interval));
+    *returnSize = 0;
+    struct Interval cur = intervals[0];
+
+    for (int i = 1; i < intervalsLen; i++) {
+        struct Interval *a = intervals + i;
+        if (cur.start <= a->start && a->start <= cur.end) {
+            merge_two(&cur, a);
+        } else {
+            ret[*returnSize] = cur;
+            (*returnSize)++;
+            cur = *a;
+        }
+    }
+
+    ret[*returnSize] = cur;
+    (*returnSize)++;
+
+    return ret;
 }
 
 int main(void)
 {
+    struct Interval intervals[] = {
+        [0] = {
+            .start = 10,
+            .end = 30,
+        },
+        [1] = {
+            .start = 20,
+            .end = 60,
+        },
+        [2] = {
+            .start = 80,
+            .end = 100,
+        },
+        [3] = {
+            .start = 150,
+            .end = 180,
+        },
+    };
+    int size;
+    struct Interval *ret = merge(intervals, 4, &size);
+
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("[%d,%d],", ret[i].start, ret[i].end);
+    }
+    printf("]\n");
+
     return 0;
 }
