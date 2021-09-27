328504dc8f42946fd0e8d7ba4f224b1e96428cd8
zeng heng <zeng.heng@wuqi-tech.com>
2021-09-27 10:11:23 +0800

    update interview2_1

--- leecode/now_coder/hw/interview_2/1.c
+++ leecode/now_coder/hw/interview_2/1.c
@@ -3,22 +3,18 @@
 
 int apple(long long num, int time)
 {
-    if (num == 1)
-        return 0;
-
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-            time++;
-        } else {
-            int t1 = apple(num + 1, time + 1);
-            int t2 = apple(num - 1, time + 1);
-            time = t1 < t2 ? t1 : t2;
-            break;
-        }
+    while (num % 2 == 0) {
+        num /= 2;
+        time++;
     }
 
-    return time;
+    if (num == 1)
+        return time;
+
+    int t1 = apple(num + 1, time + 1);
+    int t2 = apple(num - 1, time + 1);
+
+    return t1 < t2 ? t1 : t2;
 }
 
 int main(void)
