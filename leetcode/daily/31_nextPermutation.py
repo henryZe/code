9d756e3457f2068c2fc461c8937acfb261ebeade
zeng.heng <henryz_e@163.com>
2022-01-14 11:15:30 +0800

    add 31

---
+++ leetcode/daily/31_nextPermutation.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] > nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            # exchange the min num with the closest bigger one
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # reverse the rest
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+        return
+
+nums = [1,2,3]
+Solution().nextPermutation(nums)
+print(nums)
