b3ee7efac0e58d4b2f627418643bb14dabcbcfb8
Zeng Heng <henryZ_e@163.com>
2022-07-24 18:57:33 +0800

    add sortedcontainers

--- leetcode/competition/test_303/3_FoodRatings.py
+++ leetcode/competition/test_303/3_FoodRatings.py
@@ -1,19 +1,33 @@
-from Typing import List
+from typing import List
+from collections import defaultdict
+from sortedcontainers import SortedSet
 
 class FoodRatings:
-
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.food_hash = {}
+        # hash + AVL tree
+        self.cuisine_hash = defaultdict(SortedSet)
 
+        for i, food in enumerate(foods):
+            self.food_hash[food] = [cuisines[i], ratings[i]]
+            self.cuisine_hash[cuisines[i]].add((-ratings[i], food))
+
+        return
 
     def changeRating(self, food: str, newRating: int) -> None:
-
+        cuisine, rating = self.food_hash[food]
+        self.cuisine_hash[cuisine].remove((-rating, food))
+        self.cuisine_hash[cuisine].add((-newRating, food))
+        self.food_hash[food][1] = newRating
 
     def highestRated(self, cuisine: str) -> str:
+        return self.cuisine_hash[cuisine][0][1]
 
 
-
-# Your FoodRatings object will be instantiated and called as such:
-# obj = FoodRatings(foods, cuisines, ratings)
-# obj.changeRating(food,newRating)
-# param_2 = obj.highestRated(cuisine)
-
+obj = FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7])
+print(obj.highestRated("korean"))
+print(obj.highestRated("japanese"))
+obj.changeRating("sushi", 16)
+print(obj.highestRated("japanese"))
+obj.changeRating("ramen", 16)
+print(obj.highestRated("japanese"))
