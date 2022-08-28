22edc71115794da47b745bd9c2aa6668c12adaab
Zeng Heng <henryZ_e@163.com>
2022-08-28 18:04:54 +0800

    add 306 4

--- leetcode/competition/test_306/4_countSpecialNumbers.py
+++ leetcode/competition/test_306/4_countSpecialNumbers.py
@@ -1,6 +1,37 @@
+import functools
+
 class Solution:
     def countSpecialNumbers(self, n: int) -> int:
+        s = str(n)
 
-        return 0
+        @functools.lru_cache(None)
+        def dp(bit: int, mask: int, islimit: bool, isnum: bool) -> int:
+            if bit == len(s):
+                return int(isnum)
 
+            res = 0
+            # continue to be not isnum
+            if isnum == False:
+                res += dp(bit + 1, mask, False, False)
 
+            if islimit:
+                up = int(s[bit])
+            else:
+                up = 9
+            if isnum:
+                down = 0
+            else:
+                down = 1
+            for i in range(down, up + 1):
+                # not used
+                if ((1 << i) & mask) == 0:
+                    res += dp(bit + 1, mask | (1 << i), i == up and islimit, True)
+
+            return res
+
+        return dp(0, 0, True, False)
+
+# n = 20
+# n = 5
+n = 135
+print(Solution().countSpecialNumbers(n))
