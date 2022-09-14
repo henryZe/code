173a51c1faa177d5f5500f473f1787c3c9112c00
ZengHeng <henryZ_e@163.com>
2022-09-14 22:31:45 +0800

    add 310 4

--- leetcode/competition/test_310/4_lengthOfLIS.py
+++ leetcode/competition/test_310/4_lengthOfLIS.py
@@ -1,14 +1,61 @@
+from turtle import update
 from typing import List
+
+class segment_tree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (4 * n)
+        return
+
+    # O(logN)
+    def update(self, idx, l, r, num, val):
+        if l == r:
+            self.tree[idx] = val
+            return
+
+        mid = (l + r) // 2
+        if num <= mid:
+            self.update(idx * 2, l, mid, num, val)
+        else:
+            self.update(idx * 2 + 1, mid + 1, r, num, val)
+
+        self.tree[idx] = max(self.tree[idx * 2], self.tree[idx * 2 + 1])
+        return
+
+    # O(logN)
+    def query(self, idx, l, r, range_l, range_r):
+        if range_l <= l and r <= range_r:
+            return self.tree[idx]
+
+        res = 0
+        mid = (l + r) // 2
+        if range_l <= mid:
+            res = max(res, self.query(idx * 2, l, mid, range_l, range_r))
+        if mid < range_r:
+            res = max(res, self.query(idx * 2 + 1, mid + 1, r, range_l, range_r))
+        return res
 
 class Solution:
     def lengthOfLIS(self, nums: List[int], k: int) -> int:
-        
-        return 
+        n = max(nums)
+        seg = segment_tree(n)
 
-nums = [4,2,1,4,3,4,5,8,15]
-k = 3
+        # O(N * logN)
+        for num in nums:
+            if num == 1:
+                seg.update(1, 1, n, 1, 1)
+                continue
+
+            range_l = max(num - k, 1)
+            val = seg.query(1, 1, n, range_l, num - 1) + 1
+            seg.update(1, 1, n, num, val)
+
+        return seg.tree[1]
+
+# nums = [4,2,1,4,3,4,5,8,15]
+# k = 3
 # nums = [7,4,5,1,8,12,4,7]
 # k = 5
-# nums = [1,5]
-# k = 1
+nums = [1,5]
+k = 1
 print(Solution().lengthOfLIS(nums, k))
--- leetcode/competition/test_310/4_lengthOfLIS_otl.py
+++ leetcode/competition/test_310/4_lengthOfLIS_otl.py
@@ -1,46 +1,18 @@
 from typing import List
 
 class Solution:
+    # O(n^2)
     def lengthOfLIS(self, nums: List[int], k: int) -> int:
-        l = []
-        h = {}
+        n = max(nums)
+        l = [0] * (n + 1)
+        for num in nums:
+            l[num] = max(l[max(num - k, 0):num]) + 1
+        return max(l)
 
-        for num in nums[::-1]:
-            if not l:
-                l.append(1)
-                h[1] = [num]
-                continue
-
-            find = False
-            cur_long = 0
-            for long in l[::-1]:
-                cur_long = long
-
-                for value in h[cur_long]:
-                    if num < value and num + k >= value:
-                        cur_long += 1
-                        if cur_long in h:
-                            h[cur_long].append(num)
-                        else:
-                            h[cur_long] = [num]
-                        if cur_long > l[-1]:
-                            l.append(cur_long)
-                        find = True
-                        break
-
-                if find == True:
-                    break
-
-            if find == False:
-                h[1].append(num)
-                continue
-
-        return l[-1]
-
-# nums = [4,2,1,4,3,4,5,8,15]
-# k = 3
+nums = [4,2,1,4,3,4,5,8,15]
+k = 3
 # nums = [7,4,5,1,8,12,4,7]
 # k = 5
-nums = [1,5]
-k = 1
+# nums = [1,5]
+# k = 1
 print(Solution().lengthOfLIS(nums, k))
