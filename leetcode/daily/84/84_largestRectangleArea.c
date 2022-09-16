77a57fc4d832a97b1f1128bd47742b0c1fa299f6
ZengHeng <henryZ_e@163.com>
2022-09-16 15:57:45 +0800

    update 84

--- leetcode/daily/84/84_largestRectangleArea.c
+++ leetcode/daily/84/84_largestRectangleArea.c
@@ -5,29 +5,33 @@
 
 int largestRectangleArea(int* heights, int heightsSize)
 {
-    int res = 0;
-    int pre_i;
-    VosStack *s_height = VOS_StackCreate(NULL);
+    int i, cur = 0, res = 0;
     VosStack *s_pos = VOS_StackCreate(NULL);
+    int *l = malloc(heightsSize * sizeof(int));
+    int *r = malloc(heightsSize * sizeof(int));
 
-    VOS_StackPush(s_height, -1);
-    VOS_StackPush(s_pos, -1);
+    for (i = 0; i < heightsSize; i++)
+        // until to the end in default
+        r[i] = heightsSize - 1;
 
-    for (int i = 0; i < heightsSize; i++) {
-        printf("heights[i] %d, res %d\n", heights[i], res);
-        res = fmax(heights[i], res);
-
-        while (VOS_StackTop(s_height) > heights[i]) {
-            VOS_StackPop(s_height);
+    for (i = 0; i < heightsSize; i++) {
+        while (!VOS_StackEmpty(s_pos) &&
+                heights[VOS_StackTop(s_pos)] > heights[i]) {
+            r[VOS_StackTop(s_pos)] = i - 1;
             VOS_StackPop(s_pos);
         }
-        pre_i = VOS_StackTop(s_pos);
+        if (!VOS_StackEmpty(s_pos))
+            l[i] = VOS_StackTop(s_pos);
+        else
+            l[i] = -1;
 
-        printf("calc %d, res %d\n", (i - pre_i + 1) * heights[i], res);
-        res = fmax((i - pre_i + 1) * heights[i], res);
-
-        VOS_StackPush(s_height, heights[i]);
         VOS_StackPush(s_pos, i);
+    }
+
+    for (i = 0; i < heightsSize; i++) {
+        cur = (r[i] - l[i]) * heights[i];
+        printf("calc %d %d, cur %d, res %d\n", l[i], r[i], cur, res);
+        res = fmax(cur, res);
     }
 
     return res;
@@ -36,6 +40,7 @@ int largestRectangleArea(int* heights, int heightsSize)
 int main(void)
 {
     int h[] = {2,1,5,6,2,3};
+    // int h[] = {2,4};
     printf("%d\n", largestRectangleArea(h, ARRAY_SIZE(h)));
     return 0;
 }
